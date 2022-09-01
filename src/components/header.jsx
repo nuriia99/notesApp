44a38a0a045ea76304b737daa06e2606c247ba3a
Nuria Anton Calle <nuria.anton.calle@estudiantat.upc.edu>
2022-09-01 22:21:07 +0200

    navbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 function Header(){
     return <div>
-        <h1>hello world</h1>
+        <nav class="navbar bg-light">
+        <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1">Navbar</span>
+        </div>
+        </nav>
     </div>
 }
 
