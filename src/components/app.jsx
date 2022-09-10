f62ecc0557093fc9e957265b8d86e73ccd1453a5
nuria <nuria.anton.calle@estudiantat.upc.edu>
2022-09-10 11:48:13 +0200

    part1

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,11 +4,31 @@ import FloatButton from "./floatButton.jsx";
 import Card from "./card.jsx";
 import Footer from "./footer.jsx";
 
+const cards = [
+    {
+        title: "Work",
+        content: "Finish this app."
+    },
+    {
+        title: "English",
+        content: "Learn the phrasals verbs."
+    },
+    {
+        title: "University",
+        content: "Start the TFG."
+    }
+];
+
 function App(){
     return <div>
         <Header />
         <FloatButton />
-        <Card />
+        <div className="div-cards">
+        {cards.map( card => {
+            return <Card title={card.title} content={card.content} />
+        })}
+        </div>
+       
         <Footer />
     </div>
 }
