f59bf2ad6fb8a42b04a4bb303df49067a13a7951
nuria <nurianton1999@gmail.com>
2022-09-10 11:15:56 +0200

    addCard

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from './header.jsx';
 import FloatButton from "./floatButton.jsx";
 import Card from "./card.jsx";
 import Footer from "./footer.jsx";
+import AddCard from "./addCard.jsx";
 
 function App(){
+    const [cards, setCards] = useState([]);
+
     return <div>
         <Header />
-        <FloatButton />
-        <Card />
+        <AddCard />
+        {cards.map((card) =>{<Card title="hola" content="hjofe" />} )}
+        <Card title="hola" content="hjofe" />
         <Footer />
     </div>
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -4,8 +4,8 @@ function Card(props){
     return <div>
         <div class="card">
             <div class="card-body">
-                <h5 class="card-title">Work</h5>
-                <p class="card-text">Finish this App.</p>
+                <h5 class="card-title">{props.title}</h5>
+                <p class="card-text">{props.content}</p>
             </div>
         </div>
     </div>
